8157e74a278391a182c9820f138962e246366fc1
ChinnaphatLoha <chinnaphat.loha@mail.kmutt.ac.th>
2024-08-05 02:40:57 +0700

    feat: Add AuthenticationController for handling user authentication and registration

---
+++ src/main/java/com/sit/note_app/controllers/AuthenticationController.java
@@ -0,0 +1,33 @@
+package com.sit.note_app.controllers;
+
+import com.sit.note_app.dto.AuthenticationRequest;
+import com.sit.note_app.dto.AuthenticationResponse;
+import com.sit.note_app.dto.RegisterRequest;
+import com.sit.note_app.services.AuthenticationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+    private final AuthenticationService service;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(
+            @RequestBody RegisterRequest request
+    ) {
+        return ResponseEntity.ok(service.register(request));
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> authenticate(
+            @RequestBody AuthenticationRequest request
+    ) {
+        return ResponseEntity.ok(service.authenticate(request));
+    }
+}
